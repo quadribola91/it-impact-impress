cc67b7ed2da13585d1b710b3553b41962ddd2a45
Developer <quadribola91@gmail.com>
2026-01-13 08:40:22 +0100

    2026A

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -6,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { ThemeProvider } 88 "next-themes";
+import { ThemeProvider } from "next-themes";
 
 const queryClient = new QueryClient();
 
